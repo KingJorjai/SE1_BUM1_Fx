37bf9b0aec18e533faa3f4c4e637fb3d47bd8438
Jorjai <jorjai@jorjai.net>
2025-02-06 11:25:25 +0100

    another other another try of beating the checker

--- src/main/java/eus/ehu/bum1_fx/presentation/CalculatorController.java
+++ src/main/java/eus/ehu/bum1_fx/presentation/CalculatorController.java
@@ -18,75 +18,68 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-
 public class CalculatorController {
 
-        @FXML
-        private TextField amountTextField;
+    @FXML
+    private TextField amountTextField;
 
-        @FXML
-        private ComboBox<String> fromComboBox;
+    @FXML
+    private ComboBox<String> fromComboBox;
 
-        @FXML
-        private Label result;
+    @FXML
+    private Label result;
 
-        @FXML
-        private ComboBox<String> toComboBox;
+    @FXML
+    private ComboBox<String> toComboBox;
 
-        @FXML
-        void initialize() {
-            ExchangeCalculator exchangeCalculator = new BarcenaysCalculator();
+    private ExchangeCalculator bizLogic;
 
-            // initialize toComboBox
-            fromComboBox.setItems(FXCollections.observableArrayList(exchangeCalculator.getCurrencyLongNames()));
+    @FXML
+    void initialize() {
+        bizLogic = new BarcenaysCalculator();
 
-            // initialize fromComboBox
-            toComboBox.setItems(FXCollections.observableArrayList(exchangeCalculator.getCurrencyLongNames()));
+        // initialize toComboBox
+        fromComboBox.setItems(FXCollections.observableArrayList(bizLogic.getCurrencyLongNames()));
 
-            result.setBackground(new Background(
-                    new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
-        }
+        // initialize fromComboBox
+        toComboBox.setItems(FXCollections.observableArrayList(bizLogic.getCurrencyLongNames()));
 
-        @FXML
-        void onClick(ActionEvent event) {
-            ExchangeCalculator exchangeCalculator = new BarcenaysCalculator();
-            try {
-                double origAmount = Double.parseDouble(amountTextField.getText());
+        result.setBackground(new Background(
+                new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
+    }
 
-					/* Invalid numbers will be trapped in the catch block. But
-					   non-positive numbers will also be discarded */
-                if (origAmount <= 0) throw new NumberFormatException();
-                String origCurrency = fromComboBox.getValue();
-                origCurrency = origCurrency.substring(0, 3);
-                String endCurrency = toComboBox.getValue();
-                endCurrency = endCurrency.substring(0, 3);
+    @FXML
+    void onClick(ActionEvent event) {
+        try {
+            double origAmount = Double.parseDouble(amountTextField.getText());
 
-					/* If both chosen currencies are equal the online converter
-					   won't provide a result */
-                if (origCurrency.equals(endCurrency)) {
-                    result.setText("Please select different currencies");
-                } else {
-                    try {
-                        double destAmount = exchangeCalculator.getChangeValue(origCurrency, origAmount, endCurrency);
-                        destAmount -= exchangeCalculator.calculateCommission(destAmount, endCurrency);
-                        NumberFormat twoDecimal = NumberFormat.getNumberInstance(Locale.US);
-                        twoDecimal.setMaximumFractionDigits(2);
-                        twoDecimal.setRoundingMode(RoundingMode.FLOOR);
-                        if (destAmount < 0) {
-                            result.setText("Minimum commission is 3.00€");
-                            return;
-                        }
-                        result.setText(twoDecimal.format(destAmount));
+            if (origAmount <= 0) throw new NumberFormatException();
+            String origCurrency = fromComboBox.getValue();
+            origCurrency = origCurrency.substring(0, 3);
+            String endCurrency = toComboBox.getValue();
+            endCurrency = endCurrency.substring(0, 3);
 
-                    } catch (Exception e1) {
-                        // e1.printStackTrace();
-                        result.setText("Conversion could not be done");
+            if (origCurrency.equals(endCurrency)) {
+                result.setText("Please select different currencies");
+            } else {
+                try {
+                    double destAmount = bizLogic.getChangeValue(origCurrency, origAmount, endCurrency);
+                    destAmount -= bizLogic.calculateCommission(destAmount, endCurrency);
+                    NumberFormat twoDecimal = NumberFormat.getNumberInstance(Locale.US);
+                    twoDecimal.setMaximumFractionDigits(2);
+                    twoDecimal.setRoundingMode(RoundingMode.FLOOR);
+                    if (destAmount < 0) {
+                        result.setText("Minimum commission is 3.00€");
+                        return;
                     }
+                    result.setText(twoDecimal.format(destAmount));
+
+                } catch (Exception e1) {
+                    result.setText("Conversion could not be done");
                 }
-            } catch (NumberFormatException e2) {
-                result.setText("Please introduce a valid amount");
             }
+        } catch (NumberFormatException e2) {
+            result.setText("Please introduce a valid amount");
         }
-
-
+    }
 }
