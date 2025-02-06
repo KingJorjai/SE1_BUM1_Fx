16cf3b3c903314a3a451734cb6572316c40f8623
Jorjai <jorjai@jorjai.net>
2025-02-06 10:58:21 +0100

    Update parameter names in ExchangeCalculator interface to pass checks

--- src/main/java/eus/ehu/bum1_fx/business_logic/ExchangeCalculator.java
+++ src/main/java/eus/ehu/bum1_fx/business_logic/ExchangeCalculator.java
@@ -3,7 +3,7 @@ package eus.ehu.bum1_fx.business_logic;
 public interface ExchangeCalculator {
 
     String[] getCurrencyLongNames();
-    double getChangeValue(String cur1, double amount, String cur2) throws Exception;
+    double getChangeValue(String origCurrency, double amount, String endCurrency) throws Exception;
     double calculateCommission(double amount, String currency) throws Exception;
-    boolean isCurrencyValid(String currency);
+    boolean isCurrencyValid(String origCurrency);
 }
